31adb6d06a4600542187f7f8d9e089016e455d62
riverstreet <1787552935@qq.com>
2022-06-26 17:11:17 +0800

    新建GoodsList仓库

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,33 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app">
+    <MyHeader />
+    <MyGoods />
+    <MyFooter />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MyHeader from "@/components/MyHeader.vue";
+import MyGoods from "@/components/MyGoods.vue";
+import MyFooter from "@/components/MyFooter.vue";
+
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    MyHeader,
+    MyGoods,
+    MyFooter,
+  },
+};
 </script>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style  scoped>
+  .app{
+    box-sizing: border-box;
+    padding: 50px 0;
+    max-height: 100vh;
+    visibility: auto;
+  }
+</style>>
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
+import "bootstrap/dist/css/bootstrap.css" // 引入第三方包里的某个css文件
 
 Vue.config.productionTip = false
 
